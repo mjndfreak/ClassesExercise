c716c70eae19c9a77bb00f9a08986d02e5b8c36f
mjndfreak <cagdasergencc@gmail.com>
2024-08-11 18:26:40 +0300

    Delete ClassesExercise directory

--- ClassesExercise/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using ClassesExercise;
-
-Baby bebe = new Baby();
-
-Baby bebe2 = new Baby("Şükrücan ","Gürlek");
-
-Console.WriteLine("Name: " + bebe.Name + "\nSurname: " + bebe.Surname + "\nBirth time: " + bebe.BirthTime);
-Console.WriteLine("Name: " + bebe2.Name + "\nSurname: " + bebe2.Surname + "\nBirth time: " + bebe2.BirthTime);
